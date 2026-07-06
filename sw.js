bea0b92efe2480e53df47b5befea597a59e53d82
reflexionesgood <reflexionesgood@outlook.com>
2026-07-05 21:16:28 -0500

    Actualizar sitio web

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 11240716
+    "zoneId": 11240753
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
