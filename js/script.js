8c302250e8af783f7a576b5440d22e35449a9d8a
reflexionesgood <reflexionesgood@outlook.com>
2026-07-05 22:53:40 -0500

    modificando el script

--- js/script.js
+++ js/script.js
@@ -1,85 +1,25 @@
-const books = {
-  GEN: "Génesis",
-  EXO: "Éxodo",
-  LEV: "Levítico",
-  NUM: "Números",
-  DEU: "Deuteronomio",
-  JOS: "Josué",
-  JDG: "Jueces",
-  RUT: "Rut",
-  "1SA": "1 Samuel",
-  "2SA": "2 Samuel",
-  "1KI": "1 Reyes",
-  "2KI": "2 Reyes",
-  "1CH": "1 Crónicas",
-  "2CH": "2 Crónicas",
-  EZR: "Esdras",
-  NEH: "Nehemías",
-  EST: "Ester",
-  JOB: "Job",
-  PSA: "Salmos",
-  PRO: "Proverbios",
-  ECC: "Eclesiastés",
-  SNG: "Cantares",
-  ISA: "Isaías",
-  JER: "Jeremías",
-  LAM: "Lamentaciones",
-  EZK: "Ezequiel",
-  DAN: "Daniel",
-  HOS: "Oseas",
-  JOL: "Joel",
-  AMO: "Amós",
-  OBA: "Abdías",
-  JON: "Jonás",
-  MIC: "Miqueas",
-  NAM: "Nahúm",
-  HAB: "Habacuc",
-  ZEP: "Sofonías",
-  HAG: "Hageo",
-  ZEC: "Zacarías",
-  MAL: "Malaquías",
-  MAT: "Mateo",
-  MRK: "Marcos",
-  LUK: "Lucas",
-  JHN: "Juan",
-  ACT: "Hechos",
-  ROM: "Romanos",
-  "1CO": "1 Corintios",
-  "2CO": "2 Corintios",
-  GAL: "Gálatas",
-  EPH: "Efesios",
-  PHP: "Filipenses",
-  COL: "Colosenses",
-  "1TH": "1 Tesalonicenses",
-  "2TH": "2 Tesalonicenses",
-  "1TI": "1 Timoteo",
-  "2TI": "2 Timoteo",
-  TIT: "Tito",
-  PHM: "Filemón",
-  HEB: "Hebreos",
-  JAS: "Santiago",
-  "1PE": "1 Pedro",
-  "2PE": "2 Pedro",
-  "1JN": "1 Juan",
-  "2JN": "2 Juan",
-  "3JN": "3 Juan",
-  JUD: "Judas",
-  REV: "Apocalipsis"
-};
+
+const verses = [
+  "john+3:16",
+  "psalms+23:1",
+  "jeremiah+33:3",
+  "isaiah+41:10",
+  "philippians+4:13"
+];
 
 async function getVerse() {
+  const random = Math.floor(Math.random() * verses.length);
+  const verse = verses[random];
+
   try {
-    const response = await fetch("https://esbiblia.net/api/random/?v=RVR1960");
+    const response = await fetch(`https://bible-api.com/${verse}`);
     const data = await response.json();
 
-    const verse = data.verses[0];
-    const bookName = books[verse.book_id] || verse.book_id;
-
     document.getElementById("verse-box").innerText =
-      `${verse.text}\n\n${bookName} ${verse.chapter}:${verse.verse}`;
+      `${data.text} - ${data.reference}`;
   } catch (error) {
     document.getElementById("verse-box").innerText =
-      "No se pudo cargar el versículo.";
+      "Could not load verse.";
   }
 }
 
@@ -89,3 +29,4 @@ function openAd(element) {
 }
 
 getVerse();
+
